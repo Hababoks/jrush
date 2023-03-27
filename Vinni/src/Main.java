a316ab2ea1b66fe499ced69479981eb0258485aa
Hababoks <126653941+Hababoks@users.noreply.github.com>
2023-03-27 23:44:14 +0300

    Add files via upload
    the end of lesson

---
+++ Vinni/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        String caps = "if I type in caps ";
+        String usa = "сша";
+
+        //напишите тут ваш код
+        System.out.println(usa.toUpperCase());
+        System.out.println("Винни Пух".toUpperCase());
+        System.out.println(caps.toUpperCase() + "they know I mean business".toUpperCase());
+
+    }
+}
---
+++ bigger/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String bigAmount = "500";
+        String greatAmount = "100000";
+
+        int hugeAmount = Integer.parseInt(bigAmount)+Integer.parseInt(greatAmount); //напишите тут ваш код
+        System.out.println(hugeAmount);
+    }
+}
